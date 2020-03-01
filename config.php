0ee342c6394895f3b9c5eb3bed53f565a904bfc2
E3RFH <54802646+E3RFH@users.noreply.github.com>
2020-03-01 11:58:23 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyCWy6g7ABV_-DvdqkU0iAZ0ezj8cFdOplo');
+define('GJ_CODE', 'JP');
+define('SITE_NAME', 'YouTube-e3rfh');
+define('TITLENAME', 'YouTube-e3rfh');
+define('EN2DEKEY', 'TXhEBihhDkmSpeHBBrrWahoPZUyS1jPsCwJGYgMoDClTpCTkbnJLQt7QHrhy5TjoFq4hvQ9VvkUoN1xoX1oyqIurmDdwwI2pJP');
+define('EMAIL', 'nssa2799866881@gmail.com');
+?>
